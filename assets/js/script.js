e3d51010efb177c737e826f791dd80adef760267
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-01-20 18:00:48 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,23 @@ var createTaskHandler = function (event){
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+   
+    //create list item
     var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+    listItemE1.classname ="task-item";
+
+    //create div to hold task info and add to list item 
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class= 'task-name' >" + taskNameInput + "</h3><span class ='task-type'>" + taskTypeInput + "</span>";
+    // append the entire <li> to the parent <ul> 
+    listItemE1.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasksToDoE1.appendChild(listItemE1);
 };
 
