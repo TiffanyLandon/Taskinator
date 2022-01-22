a5d2583dbdb523def7094cff5039a7a2a75779bf
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-01-22 14:05:10 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
+// declare a new variable to increment by one each time a task is created
+var taskIdCounter = 0;
 
 var taskFormHandler = function (event){
 
@@ -30,6 +32,9 @@ var createTaskE1 = function (taskDataobj) {
  var listItemE1 = document.createElement("li");
  listItemE1.className ="task-item";
 
+ //add task id as a custom attribute
+ listItemE1.setAttribute("data-task-id", taskIdCounter);
+
  //create div to hold task info and add to list item 
  var taskInfoEl = document.createElement("div");
  taskInfoEl.className = "task-info";
@@ -39,10 +44,65 @@ var createTaskE1 = function (taskDataobj) {
  // append the entire <li> to the parent <ul> 
  listItemE1.appendChild(taskInfoEl);
 
+ var taskActionsE1 = createTaskActions(taskIdCounter);
+ listItemE1.appendChild(taskActionsE1);
+
+
  //add entire list item to list
  tasksToDoE1.appendChild(listItemE1);
+
+ // increase task counter for next unique id
+ taskIdCounter++;
 };
 
-// we used submit unstilled of click for a form- specific event
+// create a function to edit and delete buttons
+var createTaskActions = function(taskId){
+
+    var actionContainerE1 = document. createElement("div");
+    actionContainerE1.className = "task-actions" ;
+
+    // create ediit button
+    var editButtonE1  = document.createElement("button");
+    editButtonE1.textContent ="Edit";
+    editButtonE1.className = "btn edit-btn";
+    editButtonE1.setAttribute ("data-task-id", taskId);
+
+    actionContainerE1.appendChild (editButtonE1);
+
+    //create delete button
+    var deleteButtonE1 = document.createElement("button");
+    deleteButtonE1.textContent = "Delete";
+    deleteButtonE1.className = "btn delete-btn";
+    deleteButtonE1.setAttribute("data-task-id", taskId);
+
+    actionContainerE1.appendChild(deleteButtonE1);
+
+    // adding the select or dropdown function
+    var statusSelectE1 = document.createElement("select");
+    statusSelectE1.className = "select-status";
+    statusSelectE1. setAttribute ("name","status-change");
+    statusSelectE1.setAttribute("data-task-id", taskId);
+
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+
+    // for loop helps to execute a block of code a certain amount of times
+    for (var i = 0; i < statusChoices.length; i++) {
+        //create option element 
+        var statusOptionE1 = document. createElement("option");
+        statusOptionE1.textContent = statusChoices[i];
+        statusOptionE1.setAttribute("value", statusChoices[i]);
+
+        //append to select
+        statusSelectE1.appendChild(statusOptionE1);
+    }
+
+    actionContainerE1.appendChild(statusSelectE1);
+
+    return actionContainerE1;
+
+
+};
+    
+    // we used submit unstilled of click for a form- specific event
 formE1.addEventListener("submit", taskFormHandler);
 
